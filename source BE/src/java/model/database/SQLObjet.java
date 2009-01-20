6ac7e31f17dc2842d3e59abd182ae70c5c11469d
baudetaurelien <baudetaurelien@a6769b34-e094-11dd-84fc-932898d6254d>
2009-01-20 17:21:36 +0000

    Package database

---
+++ source BE/src/java/model/database/SQLObjet.java
@@ -0,0 +1,62 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package model.database;
+
+import java.sql.*;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+/**
+ *
+ * @author Baudet Aurélien
+ */
+public class SQLObjet {
+    private Connection conn;
+    
+     /**Constructor
+     * Sert à l'initialisation d'une connexion
+     */
+    protected SQLObjet()
+    {
+        this.conn=init();
+    }
+
+    /** Connection à la base de donnees
+    * Utilisation de l'annuaire JDNI
+    * @return Statement
+    */
+    public Connection init()
+    {
+        Statement stmt=null;
+        Connection conn=null;
+        try
+        {
+        Context initCtx = new InitialContext();
+        DataSource ds = (DataSource)initCtx.lookup("java:MySqlDS");
+        conn = ds.getConnection();
+        }
+        catch (Exception e) {System.out.println("Problème de connection : "); e.printStackTrace();}
+        System.out.println("Connection réussie\n");
+        return conn;
+    }
+    
+    /**  Fermeture connection à la base de donnees version JDNI
+     * 
+     */
+    public void close() throws Exception
+    {
+        this.conn.close();
+    }
+
+    /**  Getter connexion
+     * @return Connection
+     */
+    public Connection getConn() {
+        return conn;
+    }
+}
---
+++ source BE/src/java/model/database/SQLUser.java
@@ -0,0 +1,257 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package model.database;
+
+import java.sql.*;
+/** Classe SQLUser.
+ * Contient les requêtes pour la partie User du projet
+ * @author Baudet Aurélien
+ */
+public class SQLUser {
+       
+     /** Inscrit l'utilisateur dans la base de donnée.
+      * 
+     * Si absence de gsm, remplacer par le string null.
+     * Si absence de fixe, remplacer par le string null. 
+      * @param login
+      * @param pw
+      * @param prenom
+      * @param nom
+      * @param naissance
+      * @param mail
+      * @param sexe
+      * @param fixe
+      * @param gsm
+      * @return boolean
+      */
+    public static boolean insert(String login, String pw, String prenom, String nom, String naissance, String mail, String sexe, String fixe, String gsm)
+    {
+        boolean okay=true;
+        Statement stmt;
+        SQLObjet connexion=new SQLObjet();
+        String insert="insert into CUSTOMER (login,password,prenom,nom,naissance,mail,sexe";
+        String values="values (\""+login+"\",\""+pw+"\",\""+prenom+"\",\""+nom+"\",\""+naissance+"\",\""+mail+"\",\""+sexe+"\"";
+        if (fixe.compareTo("")!=0)
+        {
+            insert+=",tel_fixe";
+            values+=",\""+fixe+"\"";
+        }
+        if (gsm.compareTo("")!=0)
+        {
+            insert+=",tel_gsm";
+            values+=",\""+gsm+"\"";
+        }
+        insert+=") "+values+");";
+        try
+        {
+            stmt=connexion.getConn().createStatement();
+            stmt.executeUpdate(insert);
+            }
+        catch(SQLException e2)
+        {
+            System.out.println("SqlException : "+e2);
+            okay=false;
+        }
+        try {connexion.close();} catch (Exception e3) {System.out.println("Erreur fermeture"+e3);}
+        return okay;
+    }
+    
+     /** Verifie si l'utilisateur est autorisé
+      * 
+      * @param log
+      * @param pw
+      * @return boolean
+      * @throws java.lang.Exception
+      */
+    static public boolean isAllowed(String log, String pw) throws Exception
+    {
+        boolean okay=true;
+        Statement stmt;
+        SQLObjet connexion=new SQLObjet();
+        stmt=connexion.getConn().createStatement();
+        ResultSet rs=null;
+        try
+        {
+            rs=stmt.executeQuery("select * from CUSTOMER where (login=\""+log+"\" and password=\""+pw+"\")");
+            if (rs.next()==true)
+                {okay=true;}
+            else 
+                {okay=false;}
+        }
+        catch(Exception e2)
+        {
+            System.out.println("Exception"+e2);
+            okay=false;
+        }
+        try {connexion.close();} catch (Exception e3) {System.out.println("Erreur fermeture"+e3);}
+        return okay;
+    }
+
+     /** Verifie si l'utilisateur passé en argument existe dans la DB
+      * 
+      * @param log
+      * @return boolean
+      * @throws java.lang.Exception
+      */
+    static public boolean isUsed(String log) throws Exception
+    {
+        boolean okay=true;
+        Statement stmt;
+        SQLObjet connexion=new SQLObjet();
+        stmt=connexion.getConn().createStatement();
+        ResultSet rs=null;
+        try
+        {
+            rs=stmt.executeQuery("select * from CUSTOMER where login=\""+log+"\"");
+            if (rs.next()==true)
+                {okay=true;}
+            else 
+                {okay=false;}
+        }
+        catch(Exception e2)
+        {
+            System.out.println("Exception"+e2);
+            //okay=false;
+        }
+        try {connexion.close();} catch (Exception e3) {System.out.println("Erreur fermeture"+e3);}
+        return okay;
+    }
+    
+     /** Retourne la valeur de l'id_user à partir du login & mdp passés en arguments
+      * 
+      * @param log
+      * @param pw
+      * @return int
+      * @throws java.lang.Exception
+      */
+    static public int getID(String log, String pw) throws Exception
+    {
+        int id;
+        Statement stmt;
+        SQLObjet connexion=new SQLObjet();
+        stmt=connexion.getConn().createStatement();
+        ResultSet rs=null;
+        try
+        {
+            rs=stmt.executeQuery("select id_user from CUSTOMER where (login=\""+log+"\" and password=\""+pw+"\")");
+            rs.next();
+            id=Integer.parseInt(rs.getString("id_user"));
+        }
+        catch(Exception e2)
+        {
+            System.out.println("SQLException"+e2);
+            id=0;
+        }
+        try {connexion.close();} catch (Exception e3) {System.out.println("Erreur fermeture"+e3);}
+        return id;
+    }
+    
+     /** Renvoie l'ensemble des caracteristiques d'un utilisateur à partir du login & mdp passés en argument
+      * 
+      * @param log
+      * @param pw
+      * @return ResultSet
+      */
+    public ResultSet select(String log, String pw)
+    {
+        Statement stmt;
+        SQLObjet connexion=new SQLObjet();
+        ResultSet rs=null;
+        try
+        {
+            stmt=connexion.getConn().createStatement();
+            rs = stmt.executeQuery("select id_user from CUSTOMER where (login=\""+log+"\" and password=\""+pw+"\")");
+        }
+        catch(SQLException e2)
+        {
+            System.out.println("SqlException"+e2);
+        }
+        try {connexion.close();} catch (Exception e3) {System.out.println("Erreur fermeture"+e3);}
+        return rs;
+    }
+    
+    
+     /** Valide un compte créé à partir de l'ID (Validation.java)
+      * Sert lorsque l'utilisateur clique sur le lien dans son mail
+      * @param id
+      * @return boolean
+      */
+    static public boolean validAccount(int id)
+    {
+        boolean okay=true;
+        Statement stmt;
+        SQLObjet connexion=new SQLObjet();
+        try
+        {
+                stmt=connexion.getConn().createStatement();
+                stmt.executeUpdate("update CUSTOMER set valide=1 where id_user=\""+id+"\";");
+        }
+        catch(SQLException e2)
+        {
+            System.out.println("SqlException"+e2);
+            okay=false;
+        }
+        try {connexion.close();} catch (Exception e3) {System.out.println("Erreur fermeture"+e3);}
+        return okay;
+    }
+
+     /** Vérifie que le compte existe et est inactif
+      * Sert lorsque l'utilisateur clique sur le lien dans son mail (Validation.java)
+      * @param id
+      * @return boolean
+      */
+    static public boolean isValidAccountUnactive(int id)
+    {
+        boolean okay=true;
+        Statement stmt;
+        ResultSet rs=null;
+        SQLObjet connexion=new SQLObjet();
+        try
+        {
+                stmt=connexion.getConn().createStatement();
+                rs=stmt.executeQuery("select * from CUSTOMER where id_user=\""+id+"\" and valide=0;");
+                if (rs.next()==true)
+                {okay=true;}
+                else 
+                {okay=false;}
+        }
+        catch(SQLException e2)
+        {
+            System.out.println("SqlException"+e2);
+            okay=false;
+        }
+        try {connexion.close();} catch (Exception e3) {System.out.println("Erreur fermeture"+e3);}
+        return okay;
+    }
+
+     /** Vérifie qu'un utilisateur est abonné à des flux RSS à partir de son id
+      * @param id
+      * @return boolean
+      */
+    static public boolean hasRssUrl(int id)
+    {
+        boolean okay=true;
+        Statement stmt;
+        ResultSet rs=null;
+        SQLObjet connexion=new SQLObjet();
+        try
+        {
+                stmt=connexion.getConn().createStatement();
+                rs=stmt.executeQuery("select * from sabonne where id_user=\""+id+"\";");
+                if (rs.next()==true)
+                {okay=true;}
+                else 
+                {okay=false;}
+        }
+        catch(SQLException e2)
+        {
+            System.out.println("SqlException"+e2);
+            okay=false;
+        }
+        try {connexion.close();} catch (Exception e3) {System.out.println("Erreur fermeture"+e3);}
+        return okay;
+    }
+}
