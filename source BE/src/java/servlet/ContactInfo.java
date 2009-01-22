36562e03de7d10828a302350b0255a400f557157
baudetaurelien <baudetaurelien@a6769b34-e094-11dd-84fc-932898d6254d>
2009-01-22 09:22:40 +0000

    correction erreur bête...

--- source BE/src/java/servlet/ContactInfo.java
+++ source BE/src/java/servlet/ContactInfo.java
@@ -64,7 +64,7 @@ public class ContactInfo extends javax.servlet.http.HttpServlet {
     */
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException {
-        processRequest(request, response);
+        doGet(request, response);
     }
 
     /** 
