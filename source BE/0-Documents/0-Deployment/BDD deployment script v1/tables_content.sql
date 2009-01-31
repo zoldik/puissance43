68ab413812e68509fd051741d2e8b7d07291e16d
baudetaurelien <baudetaurelien@a6769b34-e094-11dd-84fc-932898d6254d>
2009-01-31 19:37:08 +0000

    added Test Customer in DB

--- source BE/0-Documents/0-Deployment/BDD deployment script v1/tables_content.sql
+++ source BE/0-Documents/0-Deployment/BDD deployment script v1/tables_content.sql
@@ -8,3 +8,10 @@ INSERT INTO `ARTICLE` (`ID_ARTICLE`, `name`, `type`, `description`, `unit_price`
 (5, 'hub a', 'hub', 'USB', 31, 120),
 (6, 'hub b', 'hub', 'RJ45', 32, 130),
 (7, 'hub c', 'hub', 'RJ45', 33, 140);
+
+--
+-- Contenu de la table `CUSTOMER`
+--
+
+INSERT INTO `CUSTOMER` (`USER_ID`, `ID_VOIP`, `ADR_ID_ADDRESS`, `ABO_ID_ABONNEMENT`, `LOGIN`, `PASSWORD`, `NOM`, `PRENOM`, `SEXE`, `SOCIETE`, `MAIL`, `USER_TYPE`, `ACCOUNT_LEVEL`, `DEBIT_VOD`, `TEL`, `GSM`, `BORN`, `USER_ID2`, `ID_ABONNEMENT`, `USER_ID3`, `ID_ADDRESS`) VALUES
+(1, 1, 0, NULL, 'UtilisateurTest', 'blablatest', 'Machin', 'Truc', 'M', 'SuperSociete', 'machintruc@supersociete.fr', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
