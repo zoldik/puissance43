e4e28f395a7284d9c32712f9bdaeae6c42f0392e
baudetaurelien <baudetaurelien@a6769b34-e094-11dd-84fc-932898d6254d>
2009-02-02 22:24:07 +0000

    Vincent : Sources packages : début du DAO pattern

---
+++ source BE/src/java/DAO/DAOFactory.java
@@ -0,0 +1,38 @@
+package DAO;
+
+/**Abstract class DAO Factory
+ *
+ * @author vincent
+ */
+public abstract class DAOFactory {
+    // List of DAO types supported by the factory
+    public static final int MYSQL = 1;
+    public static final int CLOUDSCAPE = 2;
+    public static final int ORACLE = 3;
+    public static final int SYBASE = 4;
+
+    // There will be a method for each DAO that can be 
+    // created. The concrete factories will have to 
+    // implement these methods.
+    //public abstract CustomerDAO getCustomerDAO();
+
+    //public abstract AccountDAO getAccountDAO();
+
+    //public abstract OrderDAO getOrderDAO();
+    public static DAOFactory getDAOFactory(
+            int whichFactory) {
+
+        switch (whichFactory) {
+            case MYSQL:
+                return new MySqlDAOFactory();
+            //case CLOUDSCAPE:
+                //return new CloudscapeDAOFactory();
+            //case ORACLE:
+                //return new OracleDAOFactory();
+            //case SYBASE:
+                //return new SybaseDAOFactory();
+            default:
+                return null;
+        }
+    }
+}
