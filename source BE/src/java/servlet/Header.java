749433094380da95f49728b4bd5e69f4b24ca57e
baudetaurelien <baudetaurelien@a6769b34-e094-11dd-84fc-932898d6254d>
2009-02-02 01:53:07 +0000

    +favicon +onmouseover /menu

--- source BE/src/java/servlet/Header.java
+++ source BE/src/java/servlet/Header.java
@@ -30,15 +30,15 @@ public class Header extends HttpServlet {
         
         
         out.println("<div class=\"menu\"><!-- début de la boite contenant les onglets -->"+
-                "<a class=\"onglet\" href=\"#\" onclick=\"fonctionOpenPage('accueil.jsp'); fonctionContenu('Accueil'); fonctionClean('Accueil'); fonctionMenu('Accueil'); return false;\"><div id=\"Accueil\">Accueil</div></a>"+
-                "<a class=\"onglet\" href=\"#\" onclick=\"fonctionContenu('Compte'); fonctionClean('Compte'); fonctionMenu('Compte'); return false;\"><div id=\"Compte\">Compte</div></a>"+
-                "<a class=\"onglet\" href=\"#\" onclick=\"fonctionContenu('VOD'); fonctionClean('VOD'); fonctionMenu('VOD'); return false;\"><div id=\"VOD\">VOD</div></a>"+
-                "<a class=\"onglet\" href=\"#\" onclick=\"fonctionOpenPage('voip/voip.jsp'); fonctionContenu('VoIP'); fonctionClean('VoIP'); fonctionMenu('VoIP'); return false;\"><div id=\"VoIP\">VoIP</div></a>"+
-                "<a class=\"onglet\" href=\"#\" onclick=\"fonctionContenu('WebMail'); fonctionClean('WebMail'); fonctionMenu('WebMail'); return false;\"><div id=\"WebMail\">WebMail</div></a>"+
-                "<a class=\"onglet\" href=\"#\" onclick=\"fonctionContenu('Assistance'); fonctionClean('Assistance'); fonctionMenu('Assistance'); return false;\"><div id=\"Assistance\">Assistance</div></a>"+
-                "<a class=\"onglet\" href=\"#\" onclick=\"fonctionContenu('Réseaux'); fonctionClean('Réseaux'); fonctionMenu('Réseaux'); return false;\"><div id=\"Réseaux\">Réseaux</div></a>"+
-                "<a class=\"onglet\" href=\"#\" onclick=\"fonctionContenu('NetworkDevices'); fonctionClean('NetworkDevices'); fonctionMenu('NetworkDevices'); return false;\"><div id=\"NetworkDevices\">NetworkDevices</div></a>"+
-                "<a class=\"onglet\" href=\"#\" onclick=\"fonctionContenu('NetworkSolutions'); fonctionClean('NetworkSolutions'); fonctionMenu('NetworkSolutions'); return false;\"><div id=\"NetworkSolutions\">NetworkSolutions</div></a>"+
+                "<a class=\"onglet\" href=\"#\" onclick=\"fonctionOpenPage('accueil.jsp');\" onmouseover=\"fonctionContenu('Accueil'); fonctionClean('Accueil'); fonctionMenu('Accueil'); return false;\"><div id=\"Accueil\">Accueil</div></a>"+
+                "<a class=\"onglet\" href=\"#\" onmouseover=\"fonctionContenu('Compte'); fonctionClean('Compte'); fonctionMenu('Compte'); return false;\"><div id=\"Compte\">Compte</div></a>"+
+                "<a class=\"onglet\" href=\"#\" onmouseover=\"fonctionContenu('VOD'); fonctionClean('VOD'); fonctionMenu('VOD'); return false;\"><div id=\"VOD\">VOD</div></a>"+
+                "<a class=\"onglet\" href=\"#\" onclick=\"fonctionOpenPage('voip/voip.jsp');\" onmouseover=\"fonctionContenu('VoIP'); fonctionClean('VoIP'); fonctionMenu('VoIP'); return false;\"><div id=\"VoIP\">VoIP</div></a>"+
+                "<a class=\"onglet\" href=\"#\" onmouseover=\"fonctionContenu('WebMail'); fonctionClean('WebMail'); fonctionMenu('WebMail'); return false;\"><div id=\"WebMail\">WebMail</div></a>"+
+                "<a class=\"onglet\" href=\"#\" onmouseover=\"fonctionContenu('Assistance'); fonctionClean('Assistance'); fonctionMenu('Assistance'); return false;\"><div id=\"Assistance\">Assistance</div></a>"+
+                "<a class=\"onglet\" href=\"#\" onmouseover=\"fonctionContenu('Réseaux'); fonctionClean('Réseaux'); fonctionMenu('Réseaux'); return false;\"><div id=\"Réseaux\">Réseaux</div></a>"+
+                "<a class=\"onglet\" href=\"#\" onmouseover=\"fonctionContenu('NetworkDevices'); fonctionClean('NetworkDevices'); fonctionMenu('NetworkDevices'); return false;\"><div id=\"NetworkDevices\">NetworkDevices</div></a>"+
+                "<a class=\"onglet\" href=\"#\" onmouseover=\"fonctionContenu('NetworkSolutions'); fonctionClean('NetworkSolutions'); fonctionMenu('NetworkSolutions'); return false;\"><div id=\"NetworkSolutions\">NetworkSolutions</div></a>"+
                 "<div class=\"spacer\"></div> <!-- permet le retour à la ligne à la fin des boites float --> </div>"+
                 "<div class=\"menu2\" id=\"submenu\"></div> ");
     }
