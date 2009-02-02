00b44978513ebb7eef6f780daf87748f4ce48258
baudetaurelien <baudetaurelien@a6769b34-e094-11dd-84fc-932898d6254d>
2009-02-02 22:24:07 +0000

    Vincent : Sources packages : début du DAO pattern

---
+++ source BE/src/java/DAO/MySqlDAOFactory.java
@@ -0,0 +1,35 @@
+package DAO;
+
+/**
+ *
+ * @author vincent
+ */
+public class MySqlDAOFactory extends DAOFactory {
+    
+/*    
+    public static final String DRIVER=
+    "COM.cloudscape.core.RmiJdbcDriver";
+  public static final String DBURL=
+    "jdbc:cloudscape:rmi://localhost:1099/CoreJ2EEDB";
+
+  // method to create Cloudscape connections
+  public static Connection createConnection() {
+    // Use DRIVER and DBURL to create a connection
+    // Recommend connection pool implementation/usage
+  }
+  public CustomerDAO getCustomerDAO() {
+    // CloudscapeCustomerDAO implements CustomerDAO
+    return new CloudscapeCustomerDAO();
+  }
+  public AccountDAO getAccountDAO() {
+    // CloudscapeAccountDAO implements AccountDAO
+    return new CloudscapeAccountDAO();
+  }
+  public OrderDAO getOrderDAO() {
+    // CloudscapeOrderDAO implements OrderDAO
+    return new CloudscapeOrderDAO();
+  }
+ */
+    
+    
+}
