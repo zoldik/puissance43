2d1a4d34cd3e126533c0be8a77d1742ffa59fb51
baudetaurelien <baudetaurelien@a6769b34-e094-11dd-84fc-932898d6254d>
2009-02-07 18:00:03 +0000

    Aurélien CreateMailAddress Ajout de la servlet CreateMailAddress.java

---
+++ source BE/src/java/servlet/customer/account/CreateMailAddress.java
@@ -0,0 +1,34 @@
+package servlet.customer.account;
+
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import model.database.MailDAO;
+
+
+/**
+ *
+ * @author Baudet Aurélien
+ */
+public class CreateMailAddress extends javax.servlet.http.HttpServlet {
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+		response.setContentType("text/html");
+		try{
+			//On crée la session
+			//HttpSession session = request.getSession(true);
+                        MailDAO.insert(request.getParameter("mailAddress"), request.getParameter("password"));
+                        response.sendRedirect("CreateMailAddress.jsp");
+                   }
+                catch (Exception e){
+			System.err.println("Erreur ! ! !");
+			e.printStackTrace();		
+		}
+	}
+
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+		doGet(request,response);
+	}
+}
