ee15a7412451ae5504047d2d4389a7fd841c4998
baudetaurelien <baudetaurelien@a6769b34-e094-11dd-84fc-932898d6254d>
2009-02-07 16:04:48 +0000

    Vincent : Sources

---
+++ source BE/src/java/model/database/FactoryDAO.java
@@ -0,0 +1,33 @@
+package model.database;
+
+import DAO.mySql.MySqlItemDAO;
+
+/**
+ * FactoryDAO owns methods in order to create DAO Object.
+ * Class FactoryDAO implements the design pattern Factory for create DAO objects.
+ * It contains only static methods to create DAO objects and static attributs to 
+ * reference the singleton DAO object.
+ * 
+ * @author vincent
+ */
+public class FactoryDAO {
+
+    static MySqlItemDAO ItemDAO = null;
+    static CustomerDAO CustomerDAO = null;
+    
+    
+    public static MySqlItemDAO getItemDAO() {
+        if (ItemDAO == null) {
+            ItemDAO = new MySqlItemDAO();
+        }
+        return ItemDAO;
+    }
+    
+    public static CustomerDAO getCustomerDAO() {
+        if (CustomerDAO == null) {
+            CustomerDAO = new CustomerDAO();
+        }
+        return CustomerDAO;
+    }
+    
+}
