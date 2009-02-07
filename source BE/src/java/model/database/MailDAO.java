8e8092022110df9065e4601b7abc738fcb19646c
baudetaurelien <baudetaurelien@a6769b34-e094-11dd-84fc-932898d6254d>
2009-02-07 14:44:05 +0000

    Aurélien - Modif MailDAO -> Ajout fonction suppression adresse mail

--- source BE/src/java/model/database/MailDAO.java
+++ source BE/src/java/model/database/MailDAO.java
@@ -23,7 +23,7 @@ public class MailDAO {
         try{
             stmt = connexion.getMailConn().createStatement();
             ResultSet rs = null;
-            rs = stmt.executeQuery("select * from compte where (login=\""+mail+"\")");
+            rs = stmt.executeQuery("select * from compte where login=\""+mail+"\"");
             if (rs.next() == true)
             {
                 okay = false;
@@ -31,6 +31,32 @@ public class MailDAO {
                 {
                     stmt.executeUpdate("insert into compte (login,motdepasse,emplacement) values (\""+mail+"\",\""+pw+"\",\"/home/vmail/RedNeck\")");
                     okay = true;
+                } 
+            } 
+        catch (Exception e)
+        {
+            System.out.println("Exception" + e);
+            okay = false;
+        }
+        try {connexion.closeMail();} catch (Exception e3) {System.out.println("Erreur fermeture" + e3);}
+        return okay;
+    }
+    
+    public static boolean delete(String mail, String pw){
+        boolean okay = true;
+        Statement stmt;
+        ConnectionMailDatabase connexion = new ConnectionMailDatabase();
+        try{
+            stmt = connexion.getMailConn().createStatement();
+            ResultSet rs = null;
+            rs = stmt.executeQuery("select * from compte where login=\""+mail+"\" and motdepasse=\""+pw+"\"");
+            if (rs.next() == true)
+            {
+                stmt.executeUpdate("delete from compte where login=\""+mail+"\" and motdepasse=\""+pw+"\"");
+                okay = true;
+            } else
+                {
+                    okay = false;
                 } 
             } 
         catch (Exception e)
