23c332a2448688ece596ef7aac641007004490ed
baudetaurelien <baudetaurelien@a6769b34-e094-11dd-84fc-932898d6254d>
2009-02-09 18:44:03 +0000

    Vincent : 0

--- source BE/0-Documents/0-Deployment/09-02-04 BDD deployment scripts/create_tables.sql
+++ source BE/0-Documents/0-Deployment/09-02-04 BDD deployment scripts/create_tables.sql
@@ -116,6 +116,7 @@ create table customer
    profession           varchar(20),
    company              varchar(20),
    account_level        smallint, 
+   unique (`login`)
    primary key (id_customer)
 );
 
