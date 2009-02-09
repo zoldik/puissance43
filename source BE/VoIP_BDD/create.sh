3d115b2863ce569dd079589b33bc4d334dcfec96
baudetaurelien <baudetaurelien@a6769b34-e094-11dd-84fc-932898d6254d>
2009-02-09 13:09:40 +0000

    [PLA] Delete source_BE/VoIP_BDD

--- source BE/VoIP_BDD/create.sh
+++
@@ -1,8 +0,0 @@
-#Creation de la base de donnees
-./create_database.sh
-
-#creation des tables
-./create_tables.sh
-
-#remplissage des tables
-./populate_tables.sh
--- source BE/VoIP_BDD/drop_tables.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-. ./config.sh
-
-$DB_MANAGER $USER_OPTION $USER $PASSWORD_OPTION $VERBOSE_OPTION $DATABASE < drop_tables.sql
