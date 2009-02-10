7e48f90b36fd1ed7d08968775da58f8667ebd18f
baudetaurelien <baudetaurelien@a6769b34-e094-11dd-84fc-932898d6254d>
2009-02-10 16:19:17 +0000

    Vincent : Source

--- source BE/src/java/servlet/account/CtrAccount.java
+++ source BE/src/java/servlet/account/CtrAccount.java
@@ -61,17 +61,43 @@ public class CtrAccount extends javax.servlet.http.HttpServlet {
 
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         HttpSession session = request.getSession();
-        
+
         // a faire dans le init() de la servlet 
         DAOFactory daoFactory = DAOFactory.getDAOFactory(DAOFactory.MYSQL);
-        
+
         CustomerDAOInterface customerDAO = daoFactory.getCustomerDAO();
+
+        //Creation of the transfert object customer 
+        CustomerTO customerTO = new CustomerTO();
+        //Set the attibutes of customerTO with parameters from the CreateCustomerAccount
+        customerTO.setFirstName(request.getParameter("firstName"));
+        customerTO.setLastName(request.getParameter("lastName"));
+        customerTO.setLogin(request.getParameter("login"));
+        customerTO.setPassword(request.getParameter("password"));
+        customerTO.setMail(request.getParameter("mail"));
+        customerTO.setSexe(request.getParameter("sexe"));
+        customerTO.setBirthday(request.getParameter("birthday"));
+        customerTO.setPhone(request.getParameter("phone"));
+        customerTO.setCellPhone(request.getParameter("cellPhone"));
+
+        //Creation of the transfert object customer
+        AddressTO addressTO = new AddressTO();
+        //Set the attibutes of customerTO with parameters from the CreateCustomerAccount
+        addressTO.setStreet(request.getParameter("street"));
+        addressTO.setPostalCode(request.getParameter("postalCode"));
+        addressTO.setCity(request.getParameter("city"));
+        addressTO.setCountry(request.getParameter("country"));
 
         //Check if the login is not already used
         if (customerDAO.isLoginUsed(request.getParameter("login")) == true) {
-            session.setAttribute("CreationAccount", "used");
-
+            session.setAttribute("CreateCustomerAccount", "loginUsed");
+            
+            session.setAttribute("customerBegin", customerTO);            
+            session.setAttribute("addressBegin", addressTO);
+            
             /*
+            boolean isUsed = customerDAO.isLoginUsed(request.getParameter("login"));
+            
             response.setContentType("text/html");
             PrintWriter writer = response.getWriter();
             writer.println("<html>");
@@ -79,75 +105,60 @@ public class CtrAccount extends javax.servlet.http.HttpServlet {
             writer.println("<title>Login already used</title>");
             writer.println("<head>");
             writer.println("<body>");
-            writer.println("<h1>Login already used</h1>");
+            writer.println("<h1>"+isUsed+"</h1>");
             writer.println("</body>");
             writer.println("</html");
-             */
+            */ 
 
             //si on rentre dans le if : meme si redirect le reste de la servlet est quand même executé
             //par contre pas de mise en base ??
             //C'est pourquoi il faut absolument faire des else if à chaque cas
-            response.sendRedirect("NotificationCreationAccount.jsp");
+            response.sendRedirect("CreateCustomerAccount.jsp");
         } else {
 
-            //Creation of the transfert object customer 
-            CustomerTO customerTO = new CustomerTO();
-            //Set the attibutes of customerTO with parameters from the CreateCustomerAccount
-            customerTO.setFirstName(request.getParameter("firstName"));
-            customerTO.setLastName(request.getParameter("lastName"));
-            customerTO.setLogin(request.getParameter("login"));
-            customerTO.setPassword(request.getParameter("password"));
-            customerTO.setMail(request.getParameter("mail"));
-            customerTO.setSexe(request.getParameter("sexe"));
-            customerTO.setBirthday(request.getParameter("birthday"));
-            customerTO.setPhone(request.getParameter("phone"));
-            customerTO.setCellPhone(request.getParameter("cellPhone"));
-            
-            //Creation of the transfert object customer
-            AddressTO addressTO = new AddressTO();
-            //Set the attibutes of customerTO with parameters from the CreateCustomerAccount
-            addressTO.setStreet(request.getParameter("street"));
-            addressTO.setPostalCode(request.getParameter("postalCode"));
-            addressTO.setCity(request.getParameter("city"));
-            addressTO.setCountry(request.getParameter("country"));
-            
             //On vérifie qu'il n'y a pas d'erreur dans le formulaire
             RegisterCustomerErrors erreursClient = new RegisterCustomerErrors();
             boolean isErrorCustomer = erreursClient.checkInfos(customerTO);
-            
+
             RegisterAddressErrors erreursAdresse = new RegisterAddressErrors();
             boolean isErrorAddress = erreursAdresse.checkInfos(addressTO);
-            
+
             if (isErrorCustomer == true || isErrorAddress == true) {
-                session.setAttribute("errorAccount", "errorAccount");
+                //Stipule qu'il y a une erreur
+                session.setAttribute("CreateCustomerAccount", "errorField");
 
                 session.setAttribute("errorsCustomer", erreursClient);
-                
+
                 session.setAttribute("customerBegin", customerTO);
+
+                session.setAttribute("errorsAddress", erreursAdresse);
+
+                //addressBegin : pour garder les champs déjà rentrer en mémoire
+                session.setAttribute("addressBegin", addressTO);
 
                 response.sendRedirect("CreateCustomerAccount.jsp");
             } else {
 
-            //Put in the BDD
-            String error = customerDAO.insertCustomer(customerTO);
+                //Put in the BDD
+                String error = customerDAO.insertCustomer(customerTO);
 
-            /*
-            response.setContentType("text/html");
-            PrintWriter writer = response.getWriter();
-            writer.println("<html>");
-            writer.println("<head>");
-            writer.println("<title>Hello World Application Servlet Page</title>");
-            writer.println("<head>");
-            writer.println("<body>");
-            writer.println(error);
-            writer.println("</body>");
-            writer.println("</html");
-             */
+                /*
+                response.setContentType("text/html");
+                PrintWriter writer = response.getWriter();
+                writer.println("<html>");
+                writer.println("<head>");
+                writer.println("<title>Hello World Application Servlet Page</title>");
+                writer.println("<head>");
+                writer.println("<body>");
+                writer.println(error);
+                writer.println("</body>");
+                writer.println("</html");
+                 */
 
-            session.setAttribute("CreationAccount", "registered");
+                session.setAttribute("CreateCustomerAccount", "registered");
 
-            response.sendRedirect("NotificationCreationAccount.jsp");
-            
+                response.sendRedirect("NotificationCreationAccount.jsp");
+
             }
         }
     }
