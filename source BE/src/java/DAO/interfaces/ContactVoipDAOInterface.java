b90de21bb5bbd9136557830e51715764503750d6
baudetaurelien <baudetaurelien@a6769b34-e094-11dd-84fc-932898d6254d>
2009-02-11 14:03:11 +0000

    thomas : 1 fichier transfert vers DAO Factory

---
+++ source BE/src/java/DAO/interfaces/ContactVoipDAOInterface.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package DAO.interfaces;
+import DAO.transfertObject.ContactVoipTO;
+import java.util.LinkedList;
+
+/**
+ *
+ * @author thomas
+ */
+public interface ContactVoipDAOInterface {
+    
+public boolean insert(int voip_contact_id, String titre, String categorie, String nom, String prenom, String telephone, String mail, int id_voip);
+public boolean insertContact(ContactVoipTO contact);
+public int getlastId();
+public ContactVoipTO selectbyId(String id);
+public LinkedList <ContactVoipTO> getContactbyVoipId(int id);
+public boolean deletebyId(int id);
+public LinkedList <ContactVoipTO> selectbyCategorie (String categorie);
+public boolean updateContactInfo(int id, ContactVoipTO contact);
+
+    
+    
+    
+}
