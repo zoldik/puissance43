1bebd186313a96db1b9c0b9294dd7a17f8b45d90
baudetaurelien <baudetaurelien@a6769b34-e094-11dd-84fc-932898d6254d>
2009-02-11 14:03:35 +0000

    thomas : 1 fichier transfert vers DAO Factory

---
+++ source BE/src/java/DAO/transfertObject/ContactVoipTO.java
@@ -0,0 +1,134 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package DAO.transfertObject;
+
+/**
+ *
+ * @author thomas
+ */
+public class ContactVoipTO {
+
+/**
+ * Classe de gestion des contacts (id, titre, categorie,nom,prenom,telephone,mail)
+ * @author thomas
+ */
+    public int id;
+    public String titre;
+    public String categorie;
+    public String nom;
+    public String prenom;
+    public String telephone;
+    public String mail;
+    public int id_voip;
+
+/**
+ *Constructeur par défaut de l'objet Contact
+ * @author thomas
+ */ 
+public ContactVoipTO() {
+    this.id=0;
+    this.titre="";
+    this.categorie="";
+    this.nom="";
+    this.prenom="";
+    this.telephone="";
+    this.mail="";
+    this.id_voip=0;}
+                
+ /**
+  * Constructeur Contact
+  * @param id
+  * @param titre
+  * @param categorie
+  * @param nom
+  * @param prenom
+  * @param telephone
+  * @param mail
+  * @param id_voip
+  */
+public ContactVoipTO(int id,String titre,String categorie,String nom,String prenom,String telephone,String mail,int id_voip) {
+    this.id=id;
+    this.titre=titre;
+    this.categorie=categorie;
+    this.nom=nom;
+    this.prenom=prenom;
+    this.telephone=telephone;
+    this.mail=mail;
+    this.id_voip=id_voip;        
+               }
+
+
+    public String getCategorie() {
+        return categorie;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public String getTitre() {
+        return titre;
+    }
+
+    public void setCategorie(String categorie) {
+        this.categorie = categorie;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public void setTitre(String titre) {
+        this.titre = titre;
+    }
+
+    public int getId_voip() {
+        return id_voip;
+    }
+
+    public void setId_voip(int id_voip) {
+        this.id_voip = id_voip;
+    }
+
+
+    
+    
+}
+
+
+
