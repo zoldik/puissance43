e7189be3dabce1f5e9cb53c526b2d345ccba0d15
baudetaurelien <baudetaurelien@a6769b34-e094-11dd-84fc-932898d6254d>
2009-02-15 12:08:38 +0000

    Aurélien - Modification ViewBill pour avoir le nom & prénom de la personne

--- source BE/src/java/servlet/customer/account/ViewBill.java
+++ source BE/src/java/servlet/customer/account/ViewBill.java
@@ -11,6 +11,7 @@ import javax.servlet.http.*;
 
 import DAO.factory.DAOFactory;
 import DAO.interfaces.CustomerBillDAOInterface;
+import DAO.interfaces.CustomerDAOInterface;
 import DAO.transfertObject.*;
 import java.util.LinkedList;
 /**
@@ -28,8 +29,14 @@ public class ViewBill extends javax.servlet.http.HttpServlet {
                         int idCustomer=customerObject.getId();
                         
                         DAOFactory daoFactory = DAOFactory.getDAOFactory(DAOFactory.MYSQL);
+                        
                         CustomerBillDAOInterface customerBillDAO = daoFactory.getCustomerBillDAO();
                         LinkedList BillList = customerBillDAO.getBillsByCustomerID(idCustomer);
+                        
+                        CustomerDAOInterface customerDAO = daoFactory.getCustomerDAO();
+                        CustomerTO customer = new CustomerTO() ;
+                        customer = customerDAO.findCustomerById(idCustomer);
+                        
                         //response.sendRedirect("accueil.jsp");
                         PrintWriter out = response.getWriter();
                         out.println("<html>");
@@ -38,7 +45,7 @@ public class ViewBill extends javax.servlet.http.HttpServlet {
                         while (i<BillList.size())
                         {
                             CustomerBillTO temp = (CustomerBillTO)BillList.get(i);
-                            out.println("Facture numéro : "+temp.getBillId()+"<br>Montant FAI : "+temp.getFaiAmount()+"<br>Montant VOD : "+temp.getVodAmount()+"<br>Montant VoIP : "+temp.getVoipAmount()+"<br>Montant total : "+temp.getAmount()+"<br><br>");
+                            out.println("Facture numéro : "+temp.getBillId()+"<br>"+customer.getFirstName()+"<br>"+customer.getLastName()+"<br>Montant FAI : "+temp.getFaiAmount()+"<br>Montant VOD : "+temp.getVodAmount()+"<br>Montant VoIP : "+temp.getVoipAmount()+"<br>Montant total : "+temp.getAmount()+"<br><br>");
                             i=i+1;
                         }
                         out.println("</body>");
