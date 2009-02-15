1ab916e3378afd80ec99e5e0f1f2465aadbb1d15
baudetaurelien <baudetaurelien@a6769b34-e094-11dd-84fc-932898d6254d>
2009-02-15 22:55:07 +0000

    francois - mise a jours svn

--- source BE/src/java/DAO/interfaces/LineDAOInterface.java
+++ source BE/src/java/DAO/interfaces/LineDAOInterface.java
@@ -14,15 +14,19 @@ import java.util.LinkedList;
  */
 public interface LineDAOInterface {
 
-    public boolean insertLine(String id,String name,String host,String nat,String type,String accountcode,String amaflags,String calllimit,String callgroup,String callerid,String cancallforward,String canreinvite,String context,String defaultip,String dtmfmode,String fromuser,String fromdomain,String insecure,String language,String mailbox,String md5secret,String deny,String permit,String mask,String musiconhold,String pickupgroup,String qualify,String regexten,String restrictcid,String rtptimeout,String rtpholdtimeout,String secret,String setvar,String disallow,String allow,String fullcontact,String ipaddr,String port,String regserver,String regseconds,String username,Boolean visible, String customerid);
+    public boolean insertLine(String id,String name,String host,String nat,String type,String accountcode,String amaflags,String calllimit,String callgroup,String callerid,String cancallforward,String canreinvite,String context,String defaultip,String dtmfmode,String fromuser,String fromdomain,String insecure,String language,String mailbox,String md5secret,String deny,String permit,String mask,String musiconhold,String pickupgroup,String qualify,String regexten,String restrictcid,String rtptimeout,String rtpholdtimeout,String secret,String setvar,String disallow,String allow,String fullcontact,String ipaddr,String port,String regserver,String regseconds,String username,Boolean visible,String customerid,String id_customer);
 
+    public boolean updateLineById(String id,String name,String host,String nat,String type,String accountcode,String amaflags,String calllimit,String callgroup,String callerid,String cancallforward,String canreinvite,String context,String defaultip,String dtmfmode,String fromuser,String fromdomain,String insecure,String language,String mailbox,String md5secret,String deny,String permit,String mask,String musiconhold,String pickupgroup,String qualify,String regexten,String restrictcid,String rtptimeout,String rtpholdtimeout,String secret,String setvar,String disallow,String allow,String fullcontact,String ipaddr,String port,String regserver,String regseconds,String username,String visible,String customerid,String id_customer);
+    
     public boolean deleteLine(String id);
 
-    public LineTO findLine(String id);
+    public LineTO findLineById(String id);
 
     public boolean updateLine(String id, String name, String value);
 
     public LinkedList<LineTO> selectAllLineTO();
+    
+    public LinkedList<LineTO> findLineByAttribute(String name, String value);
     
     
     
--- source BE/src/java/servlet/voip/doVoipLigne.java
+++ source BE/src/java/servlet/voip/doVoipLigne.java
@@ -6,6 +6,17 @@
 package servlet.voip;
 
 import model.voip.*;
+
+import model.voip.Annuaire;
+import model.voip.RowAnnuaire;
+import DAO.interfaces.LineDAOInterface;
+import DAO.interfaces.CustomerDAOInterface;
+import DAO.transfertObject.LineTO;
+import DAO.transfertObject.CustomerTO;
+import DAO.factory.DAOFactory;
+import DAO.factory.MySqlDAOFactory;
+import DAO.transfertObject.CustomerTO;
+
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
@@ -32,9 +43,57 @@ public class doVoipLigne extends HttpServlet {
     }
     
     private boolean edit(HttpServletRequest request,HttpServletResponse response) {
+       
         
-        
-        return false;
+      String id = request.getParameter("id");
+      String customerid = request.getParameter("customerid");
+      String name = request.getParameter("name"); 
+      String host = request.getParameter("host");
+      String nat = request.getParameter("nat");
+      String type = request.getParameter("type");
+      String accountcode = request.getParameter("accountcode");
+      String amaflags = request.getParameter("amaflags");
+      String calllimit = request.getParameter("calllimit");
+      String callgroup = request.getParameter("callgroup");
+      String callerid = request.getParameter("callerid");
+      String cancallforward = request.getParameter("cancallforward");
+      String canreinvite = request.getParameter("canreinvite");
+      String context = request.getParameter("context");
+      String defaultip = request.getParameter("defaultip");
+      String dtmfmode = request.getParameter("dtmfmode");
+      String fromuser = request.getParameter("fromuser");
+      String fromdomain = request.getParameter("fromdomain");
+      String insecure = request.getParameter("insecure");
+      String language = request.getParameter("language");
+      String mailbox = request.getParameter("mailbox");
+      String md5secret = request.getParameter("md5secret");
+      String deny = request.getParameter("deny");
+      String permit = request.getParameter("permit");
+      String mask = request.getParameter("mask");
+      String musiconhold = request.getParameter("musiconhold");
+      String pickupgroup = request.getParameter("pickupgroup");
+      String qualify = request.getParameter("qualify");
+      String regexten = request.getParameter("regexten");
+      String restrictcid = request.getParameter("restrictcid");
+      String rtptimeout = request.getParameter("rtptimeout");
+      String rtpholdtimeout = request.getParameter("rtpholdtimeout");
+      String secret = request.getParameter("secret");
+      String setvar = request.getParameter("setvar");
+      String disallow = request.getParameter("disallow");
+      String allow = request.getParameter("allow");
+      String fullcontact = request.getParameter("fullcontact");
+      String ipaddr = request.getParameter("ipaddr");
+      String port = request.getParameter("port");
+      String regserver = request.getParameter("regserver");
+      String regseconds = request.getParameter("regseconds");
+      String username = request.getParameter("username");
+      String visible = request.getParameter("visible");
+      String id_customer = request.getParameter("customerid");
+      
+      DAOFactory daoFactory = DAOFactory.getDAOFactory(DAOFactory.MYSQL);
+      LineDAOInterface LineDAO = daoFactory.getLineDAO();
+      
+      return LineDAO.updateLineById(id,name,host,nat,type,accountcode,amaflags,calllimit,callgroup,callerid,cancallforward,canreinvite,context,defaultip,dtmfmode,fromuser,fromdomain,insecure,language,mailbox,md5secret,deny,permit,mask,musiconhold,pickupgroup,qualify,regexten,restrictcid,rtptimeout,rtpholdtimeout,secret,setvar,disallow,allow,fullcontact,ipaddr,port,regserver,regseconds,username,visible,customerid,id_customer);
     }
     
     private boolean delete(String id) {
@@ -74,9 +133,9 @@ public class doVoipLigne extends HttpServlet {
                String id = request.getParameter("id");
                
                 if ( edit(request,response) == true) {
-                    out.println("<p align=\"center\">Add successfull !<br/>");
+                    out.println("<p align=\"center\">Edit successfull !<br/>");
                 }else{
-                    out.println("<p align=\"center\">Add failed !<br/>");  
+                    out.println("<p align=\"center\">Edit failed !<br/>");  
                 }
                 out.println( "<a href=\"./voip/admin/ligneManagement.jsp\">back</a></p>" );
            }
